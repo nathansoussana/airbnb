b20afac49706c8b44d32ca35b5b535253144ca3c
Anton Loof <antonloof@telia.com>
2023-06-05 15:18:55 +0200

    authorization added through pundit. User can only edit and delete their own equipments

---
+++ app/policies/equipment_policy.rb
@@ -0,0 +1,33 @@
+class EquipmentPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    def resolve
+      scope.all
+    end
+  end
+
+  def show?
+    true
+  end
+
+  def new?
+    create?
+  end
+
+  def create?
+    true
+  end
+
+  def edit?
+    update?
+  end
+
+  def update?
+    user == record.user
+  end
+
+  def destroy?
+    user == record.user
+  end
+
+end
