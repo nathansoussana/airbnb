7d1ec76d39e248f729d715fe2e27a696b50a7b2d
Nathan Soussana <nathan.soussana@gmail.com>
2023-06-07 15:45:18 +0200

    Created Booking model, added dependencies in Equipment and User model

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,5 +1,6 @@
 class Equipment < ApplicationRecord
   belongs_to :user
+  has_one :booking, dependent: :destroy
   validates :sport, presence: true
   validates :description, presence: true
   validates :price, presence: true
