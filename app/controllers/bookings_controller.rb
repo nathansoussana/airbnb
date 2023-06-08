2a2d8334c65f6f992f517531a388fc3e59472823
Nathan Soussana <nathan.soussana@gmail.com>
2023-06-08 17:11:30 +0200

    Created bookings#new and bookings#create actions, need to add functionality in view

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,26 @@
 class BookingsController < ApplicationController
+  before_action :set_equipment, only: [:new, :create]
+
+  def new
+    @booking = Booking.new
+    authorize @booking
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.equipment = @equipment
+    @booking.save
+    redirect_to equipment_path(@equipment)
+    authorize @booking
+  end
+
+  private
+
+  def set_equipment
+    @equipment = Equipment.find(params[:equipment_id])
+    
+  end
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
 end
